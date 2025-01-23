14651c62da8663d97a8fd1143f6562a38e24c5d0
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2025-01-24 01:04:42 +0900

    Migrate model downloading to kokoro-onnx

--- modules/kokoro/model_downloader.py
+++ modules/kokoro/model_downloader.py
@@ -1,30 +1,37 @@
 from huggingface_hub import snapshot_download
+import requests
 import os
 
-KOKORO_MODELS_REPO_ID_MAP = {
-    "Kokoro-82M": "hexgrad/Kokoro-82M"
+KOKORO_MODELS_URL = {
+    "kokoro-v0_19": {
+        "model": "https://github.com/thewh1teagle/kokoro-onnx/releases/download/model-files/kokoro-v0_19.onnx",
+        "voice": "https://github.com/thewh1teagle/kokoro-onnx/releases/download/model-files/voices.bin"
+    }
 }
 
 
 def download_model_if_no_exists(
-    repo_id: str,
+    model_name: str,
     download_dir: str
 ):
+    model_url = KOKORO_MODELS_URL.get(model_name, None).get("model", None)
+    if model_url is None:
+        raise ValueError(f"Model {model_name} not found in the list of available models.")
+    voice_url = KOKORO_MODELS_URL.get(model_name, None).get("voice", None)
+    download_file(model_url, os.path.join(download_dir, f"{model_name}.onnx"))
+
+    if voice_url is not None:
+        download_file(voice_url, os.path.join(download_dir, "voices.bin"))
+
+
+def download_file(url: str, save_path: str):
+    """Download a file from a URL to a specified path."""
     try:
-        simplified_name = repo_id
-        _id, _name = repo_id.split("/")
-        structured_name = f"models--{_id}--{_name}"
-        simplified_path, structured_path = os.path.join(download_dir, simplified_name), os.path.join(download_dir, structured_name)
-        if os.path.exists(download_dir) and os.listdir(simplified_path):
-            return True
-
-        if os.path.exists(download_dir) and os.listdir(structured_path):
-            return True
-
-        return snapshot_download(repo_id=repo_id,
-                                 allow_patterns=['*.pt', '*.pth', '*.yml', "*.json"],
-                                 cache_dir=download_dir,
-                                 resume_download=True)
-    except Exception as e:
-        raise ValueError(f"Failed to download model. Check if repo_id is valid. Error : {e}")
-
+        response = requests.get(url, stream=True)
+        response.raise_for_status()
+        with open(save_path, "wb") as f:
+            for chunk in response.iter_content(chunk_size=8192):
+                f.write(chunk)
+        print(f"Downloaded: {save_path}")
+    except requests.exceptions.RequestException as e:
+        print(f"Failed to download {url} to \"{save_path}\": {e}")
